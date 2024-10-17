17ae86ede90e5eec3f4d8e633301856384cd4242
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-17 23:32:38 +0700

    Frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+export default function App() {
+	return (
+		<div>
+			<Button className='text-red-500'>Click Me!</Button>
+		</div>
+	)
+}
