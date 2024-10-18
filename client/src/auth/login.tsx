aafdd325746b08aacd054c7cf0e02595c10f1deb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-18 11:21:31 +0700

    Zod schema for user authentication

--- client/src/auth/login.tsx
+++ client/src/auth/login.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { LockKeyhole, Mail } from 'lucide-react'
+import { Loader2, LockKeyhole, Mail } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
+import { LoginInputState, userLoginSchema } from '@/schema/userSchema'
 
 export default function LoginPage() {
 	const [input, setInput] = useState({
@@ -12,16 +13,34 @@ export default function LoginPage() {
 		password: '',
 	})
 
+	const [errors, setErrors] = useState<Partial<LoginInputState>>({})
+
 	const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target
 		setInput({ ...input, [name]: value })
 	}
 
+	const loginSubmitHandler = async (e: FormEvent) => {
+		e.preventDefault()
+		const result = userLoginSchema.safeParse(input)
+		if (!result.success) {
+			const fieldErrors = result.error.formErrors.fieldErrors
+			setErrors(fieldErrors as Partial<LoginInputState>)
+			return
+		}
+		console.log(input)
+	}
+
+	const loading = false
+
 	return (
 		<div className='flex items-center justify-center min-h-screen'>
-			<form className='md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4'>
+			<form
+				onSubmit={loginSubmitHandler}
+				className='md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4'
+			>
 				<div className='mb-4'>
-					<h1 className='font-bold text-2xl'>SmartEats</h1>
+					<h1 className='font-bold text-2xl'>PatelEats</h1>
 				</div>
 				<div className='mb-4'>
 					<div className='relative'>
@@ -30,10 +49,13 @@ export default function LoginPage() {
 							placeholder='Email'
 							name='email'
 							value={input.email}
-							className='pl-10 focus-visible:ring-1'
 							onChange={changeEventHandler}
+							className='pl-10 focus-visible:ring-1'
 						/>
 						<Mail className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.email}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-4'>
@@ -43,16 +65,25 @@ export default function LoginPage() {
 							placeholder='Password'
 							name='password'
 							value={input.password}
-							className='pl-10 focus-visible:ring-1'
 							onChange={changeEventHandler}
+							className='pl-10 focus-visible:ring-1'
 						/>
 						<LockKeyhole className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.password}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-10'>
-					<Button type='submit' className='w-full text-blue-500 '>
-						Login
-					</Button>
+					{loading ? (
+						<Button disabled className='w-full text-blue-500'>
+							<Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait
+						</Button>
+					) : (
+						<Button type='submit' className='w-full text-blue-500'>
+							Login
+						</Button>
+					)}
 					<div className='mt-4'>
 						<Link
 							to='/forgot-password'
--- client/src/auth/signup.tsx
+++ client/src/auth/signup.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { LockKeyhole, Mail, PhoneOutgoing, User } from 'lucide-react'
+import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from 'lucide-react'
 
+import { SignupInputState, userSignupSchema } from '@/schema/userSchema'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
@@ -9,20 +10,41 @@ import { Separator } from '@/components/ui/separator'
 // typescript me type define krne ka 2 trika hota hai
 
 export default function SignupPage() {
-	const [input, setInput] = useState({
+	const [input, setInput] = useState<SignupInputState>({
 		fullname: '',
 		email: '',
 		password: '',
 		contact: '',
 	})
+
+	const [errors, setErrors] = useState<Partial<SignupInputState>>({})
+
 	const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target
 		setInput({ ...input, [name]: value })
 	}
 
+	const loginSubmitHandler = async (e: FormEvent) => {
+		e.preventDefault()
+		// form validation check start
+		const result = userSignupSchema.safeParse(input)
+
+		if (!result.success) {
+			const fieldErrors = result.error.formErrors.fieldErrors
+			setErrors(fieldErrors as Partial<SignupInputState>)
+			return
+		}
+		// login api implementation start here
+		console.log(input)
+	}
+	const loading = false
+
 	return (
 		<div className='flex items-center justify-center min-h-screen'>
-			<form className='md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4'>
+			<form
+				className='md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4'
+				onSubmit={loginSubmitHandler}
+			>
 				<div className='mb-4'>
 					<h1 className='font-bold text-2xl'>SmartEats</h1>
 				</div>
@@ -37,6 +59,9 @@ export default function SignupPage() {
 							onChange={changeEventHandler}
 						/>
 						<User className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.fullname}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-4'>
@@ -50,6 +75,9 @@ export default function SignupPage() {
 							onChange={changeEventHandler}
 						/>
 						<Mail className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.email}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-4'>
@@ -63,6 +91,9 @@ export default function SignupPage() {
 							onChange={changeEventHandler}
 						/>
 						<LockKeyhole className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.password}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-4'>
@@ -76,12 +107,21 @@ export default function SignupPage() {
 							onChange={changeEventHandler}
 						/>
 						<PhoneOutgoing className='absolute inset-y-2 left-2 text-gray-500 pointer-events-none' />
+						{errors && (
+							<span className='text-xs text-red-500'>{errors.contact}</span>
+						)}
 					</div>
 				</div>
 				<div className='mb-10'>
-					<Button type='submit' className='w-full text-blue-500'>
-						Signup
-					</Button>
+					{loading ? (
+						<Button disabled className='w-full text-blue-500'>
+							<Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait
+						</Button>
+					) : (
+						<Button type='submit' className='w-full text-blue-500'>
+							Signup
+						</Button>
+					)}
 				</div>
 				<Separator />
 				<p className='mt-2'>
