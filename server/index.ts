60e3fe0fe52ffbf9a27ec091d687667f22136e7a
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-19 20:56:03 +0700

    Create user API

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 dotenv.config()
-import connectToDB from './db/connect-to-db'
+import connectToDB from './models/db/connect-to-db'
+import userRouter from './routes/user.route'
 
 const app = express()
 const PORT = process.env.PORT || 8000
+const corsOptions = {
+	origin: 'http://localhost:5173',
+	credentials: true,
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.use('/api/v1/user', userRouter)
 
 app.listen(PORT, async () => {
 	await connectToDB()
---
+++ server/routes/user.route.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+
+import { isAuthenticated } from '../lib/middlewares/is-authenticated'
+import {
+	checkAuth,
+	forgotPassword,
+	login,
+	logout,
+	resetPassword,
+	signup,
+	updateProfile,
+	verifyEmail,
+} from '../controllers/user.controller'
+
+const router = express.Router()
+
+router.route('/check-auth').get(isAuthenticated, checkAuth)
+router.route('/signup').post(signup)
+router.route('/login').post(login)
+router.route('/logout').post(logout)
+router.route('/verify-email').post(verifyEmail)
+router.route('/forgot-password').post(forgotPassword)
+router.route('/reset-password/:token').post(resetPassword)
+router.route('/profile/update').put(isAuthenticated, updateProfile)
+
+export default router
